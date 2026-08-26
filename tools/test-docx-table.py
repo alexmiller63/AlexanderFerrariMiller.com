dc2fe92d3cdcbef1154dc0f118bfdf5cdc726eb9
alexmiller63 <46362648+alexmiller63@users.noreply.github.com>
2026-08-27 05:16:38 +0530

    Update test-docx-table.py

--- tools/test-docx-table.py
+++ tools/test-docx-table.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 
 from docx import Document
+from docx.enum.text import WD_ALIGN_PARAGRAPH
 
 
 ROOT = Path(__file__).resolve().parents[1]
@@ -15,8 +16,13 @@ def main():
     table = document.add_table(rows=1, cols=2)
     table.style = "Table Grid"
 
-    table.cell(0, 0).text = "Left cell Kilroy was here"
-    table.cell(0, 1).text = "Right cell Kilroy was here"
+    left_cell = table.cell(0, 0)
+    right_cell = table.cell(0, 1)
+
+    left_cell.text = "Left cell Kilroy was here"
+    right_cell.text = "Right cell Kilroy was here"
+
+    right_cell.paragraphs[0].alignment = WD_ALIGN_PARAGRAPH.RIGHT
 
     document.save(OUTPUT_FILE)
 
