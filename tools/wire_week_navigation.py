880cdc17e964ede2578cce1ddaf439c44c9ee78e
alexmiller63 <46362648+alexmiller63@users.noreply.github.com>
2026-09-11 00:07:20 +0530

    Fix bottom Almanack navigation order

--- tools/wire_week_navigation.py
+++ tools/wire_week_navigation.py
@@ -210,8 +210,8 @@ def ensure_year_bottom_target(text: str, year: int) -> str:
     text = EMPTY_BOTTOM_RE.sub("", text)
     block = (
         f'<div class="almanack-bottom-nav-wrap" id="{BOTTOM_ID}">'
-        f'{build_year_nav(year, weekly_page=False, bottom=True)}'
         f'{site_nav}'
+        f'{build_year_nav(year, weekly_page=False, bottom=True)}'
         '</div>'
     )
     if '</aside>' in text:
